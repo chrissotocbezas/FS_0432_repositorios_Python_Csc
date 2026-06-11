552a14f173c25cc1e4e7abf7e35f1fa87607588b
chrissotocbezas <christopher.sotocabezas@ucr.ac.cr>
2026-06-10 23:19:47 -0600

    Agregar funciones de solución exacta y función f(x,y) al programa en Python.

--- poisson_2d_gauss_seidel.py
+++ poisson_2d_gauss_seidel.py
@@ -4,3 +4,15 @@ import matplotlib.pyplot as plt
 # Se proceden a importar las librerías necesarias para el método de Guass-Seidel. 
 # Se proceden a invocar funciones de la misma forma que se hizo para el método de poisson_2d_jacobi
 # Gracias a lo anterior, podremos definir el algoritmo para ejecutar el método. 
+
+# Procedemos a definir la función fuente
+
+def f(x, y):
+    return np.sin(np.pi * x) * np.sin(np.pi *y)
+
+def solucion_exacta(x, y):
+    return -np.sin(np.pi * x) * np.sin(np.pi * y) / (2.0 * (np.pi)**2)
+
+# En la función anterior, procedimos a definir la solución exacta de la función
+
+# Procedemos a ejecutar el método de Gauss-Seidel
