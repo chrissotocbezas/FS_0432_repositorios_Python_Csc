c99f9890cf85dcac02f6686980e0897a85e4a6bc
chrissotocbezas <christopher.sotocabezas@ucr.ac.cr>
2026-06-10 23:14:57 -0600

    Agregar librerías y algunos comentarios al archivo de gauss_seidel.

---
+++ poisson_2d_gauss_seidel.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Se proceden a importar las librerías necesarias para el método de Guass-Seidel. 
+# Se proceden a invocar funciones de la misma forma que se hizo para el método de poisson_2d_jacobi
+# Gracias a lo anterior, podremos definir el algoritmo para ejecutar el método. 
