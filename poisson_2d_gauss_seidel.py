2c9f52a6a70d3a5d818aa3fe0e6ccbf6dad7d173
chrissotocbezas <christopher.sotocabezas@ucr.ac.cr>
2026-06-11 20:52:42 -0600

    Generar arreglos matriciales y generar espacios de matrices con ceros para almacenar el bloque completo, generar errores de convergencia.

--- poisson_2d_gauss_seidel.py
+++ poisson_2d_gauss_seidel.py
@@ -34,3 +34,47 @@ def graficar_mapa(u, titulo, nombre_archivo, cmap):
     plt.tight_layout()
     plt.savefig(nombre_archivo, dpi=200)
     plt.close()
+
+def graficar_error_convergencia(errores, nombre_archivo):
+    plt.figure(figsize=(6, 4))
+    plt.plot(errores, color="green") # Procedemos a cambiar el color de la gráfica
+    plt.yscale("log") # Escala logarítmica para observar el decrecimiento exponencial del error
+    plt.xlabel("Iteracion")
+    plt.ylabel("Error de convergencia")
+    plt.title("Error de convergencia (Gauss-Seidel)")
+    plt.tight_layout()
+    plt.savefig(nombre_archivo, dpi=200)
+    plt.close()
+
+def main():
+    N = 50 # Colocamos el número de iteraciones
+    h = 1.0/N # Colocamos el paso
+    tol = 1e-8
+    max_iter = 200000
+
+    # Colocamos el dominio para x y y
+
+    x = np.linspace(0.0, 1.0. N+1) #N + 1 se realiza por el tema del paso
+    y = np.linspace(0.0, 1.0, N+1)
+
+    X, Y = np.meshgrid(x, y, indexing="ij")
+
+    f = f(x,y)
+    u_exacta = solucion_exacta(X,Y) # El arreglo para X y Y se utiliza para la solución u_exacta
+
+    # NOTA: En Gauss-Seidel no requerimos u_new para el almacenamiento del bloque completo, ya que las actualizaciones ocurren sobre la misma matriz 'u'
+
+    u = np.zeros((N+1, N+1))
+
+    errores_convergencia = [] # Agregamos una lista en blanco para generar un lugar donde se pueda almacenar los arreglos de conveiencia
+
+    iteracion = 0
+    error_conv = 1.0
+
+    while error_conv > tol and iteracion < max_iter:
+        # Procedemos a guardar una copia exacta para evaluar la convergencia al final del paso completo
+
+        u_old = u.copy()
+
+        # Procedemos a realizar una actualización fila por fila consecutiva para logar un Gauss-Seidel real y donde apliquemos una formulación específica para vectores
+        # Esto lo podemos conseguir mediante un bucle for
